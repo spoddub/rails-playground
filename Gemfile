e496d17eaddb64feb39cbe11c83a6df9b810f554
Sergei Poddubny <108150454+spoddub@users.noreply.github.com>
2025-09-10 17:00:59 +0300

    add first app

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# Gemfile
+source 'https://rubygems.org'
+
+ruby '3.4.5'
+
+gem 'rack', '~> 2.2'
+gem 'thin', '~> 2.0'
---
+++ app.rb
@@ -0,0 +1,11 @@
+# app.rb
+require 'rack'
+require 'thin'
+
+class MyApp
+  def call(env)
+    [200, { 'Content-Type' => 'text/html; charset=utf-8' }, ['Hello!']]
+  end
+end
+
+Thin::Server.start('127.0.0.1', 8080, MyApp.new)
